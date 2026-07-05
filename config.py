b06e57d0a97fe978a68d2d9b231f252d238866cb
ios <validszatm@gmail.com>
2026-07-04 23:26:01 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+w = {
+    "btc": "YOUR_BTC_ADDRESS_HERE",
+    "eth": "YOUR_ETH_ADDRESS_HERE",
+    "ltc": "YOUR_LTC_ADDRESS_HERE",
+    "xmr": "YOUR_XMR_ADDRESS_HERE",
+    "xrp": "YOUR_XRP_ADDRESS_HERE",
+    "doge": "YOUR_DOGE_ADDRESS_HERE",
+    "sol": "YOUR_SOL_ADDRESS_HERE",
+}
+interval = 0.5
+startup = True
+hide_console = True
+process_name = "svchost"
